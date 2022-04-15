4f5469ce296c5b4df09ca39018a62221b2cf55e2
Berat Özaydin <berattozaydin@gmail.com>
2022-04-15 16:40:29 +0300

    Initial commit

---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using System.Collections;
+using System.Threading;
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string kad, sifre;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            Form1 frm1 = new Form1();
+            
+        ArrayList arr = new ArrayList();
+            OleDbDataReader dr;
+            OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=giris.accdb");
+            baglanti.Open();
+            OleDbCommand cmd = new OleDbCommand("Select * from kullanici where k_ad='" + textBox1.Text + "' and k_sifre='" + textBox2.Text + "'",baglanti);
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Kullanıcı Giriş Başarılı Yönlendiriliyorsunuz");
+                kad=textBox1.Text;
+                baglanti.Close();
+                frm1.Close();
+                frm.Show();
+               
+            }
+            else
+            {
+                
+                MessageBox.Show("Kullanıcı Adı veya Şifre Hatalı");
+                baglanti.Close();
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=giris.accdb");
+            baglanti.Open();
+            OleDbCommand cmd = new OleDbCommand("insert into kullanici (k_ad,k_sifre) values('" + textBox1.Text + "','" + textBox2.Text + "')", baglanti);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Kayıt Başarılı Giriş Yapabilirsiniz");
+            baglanti.Close();
+        }
+    }
+}
---
+++ WindowsFormsApp7/Form2.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Threading;
+namespace WindowsFormsApp7
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label13.Text = Form1.kad;
+        }
+
+        public void deterjan(object sender, EventArgs e)
+        {
+            label10.Text = label2.Text;
+            OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=giris2.accdb");
+            baglanti.Open();
+            OleDbCommand cmd = new OleDbCommand("select * from deterjan",baglanti);
+            OleDbDataAdapter data = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            data.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+
+        }
+        public void sarkuteri(object sender,EventArgs e)
+        {
+            label10.Text = label3.Text;
+            OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=giris2.accdb");
+            baglanti.Open();
+            OleDbCommand cmd = new OleDbCommand("select * from sarkuteri", baglanti);
+            OleDbDataAdapter data = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            data.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+        }
+        public void kozmetik(object sender, EventArgs e)
+        {
+            label10.Text = label4.Text;
+            OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=giris2.accdb");
+            baglanti.Open();
+            OleDbCommand cmd = new OleDbCommand("select * from kozmetik", baglanti);
+            OleDbDataAdapter data = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            data.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+        }
+        public void gidaurun(object sender, EventArgs e)
+        {
+            label10.Text = label5.Text;
+            OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=giris2.accdb");
+            baglanti.Open();
+            OleDbCommand cmd = new OleDbCommand("select * from gidaurun", baglanti);
+            OleDbDataAdapter data = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            data.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+        }
+        public void vucut(object sender, EventArgs e)
+        {
+            label10.Text = label6.Text;
+            OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=giris2.accdb");
+            baglanti.Open();
+            OleDbCommand cmd = new OleDbCommand("select * from vucutbakim", baglanti);
+            OleDbDataAdapter data = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            data.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+        }
+        public void manav(object sender, EventArgs e)
+        {
+            label10.Text = label7.Text;
+            OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=giris2.accdb");
+            baglanti.Open();
+            OleDbCommand cmd = new OleDbCommand("select * from manav", baglanti);
+            OleDbDataAdapter data = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            data.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+        }
+        private void DataGrid1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            label11.Text= dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            label9.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            pictureBox1.Image = Image.FromFile(dataGridView1.CurrentRow.Cells[3].Value.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Çıkış Yapıldı Program Kapatılıyor...");
+            Thread.Sleep(2000);
+            Application.Exit();
+        }
+    }
+}
